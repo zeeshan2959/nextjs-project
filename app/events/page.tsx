d446d85eb3367a3d73e2460fc9d9ab5732a266d5
Muhammad Zeeshan <muhammadzeeshan4902959@gmail.com>
2026-03-04 23:57:59 +0500

    remove extra code

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -4,54 +4,17 @@ import ExploreBtn from "@/components/ExploreBtn";
 import { useEffect, useState } from "react";
 
 
-// const events = [
-//   {
-//     title: "Event 1",
-//     image: "/images/event1.png",
-//     date: "2026-03-03",
-//     time: "10:00 AM",
-//     slug: "event-1",
-//     location: "New York, NY",
-
-//   },
-//   {
-//     title: "Event 2",
-//     image: "/images/event2.png",
-//     date: "2026-03-03",
-//     time: "10:00 AM",
-//     slug: "event-2",
-//     location: "New York, NY",
-//   },
-//   {
-//     title: "Event 3",
-//     image: "/images/event3.png",
-//     date: "2026-03-03",
-//     time: "10:00 AM",
-//     slug: "event-3",
-//     location: "New York, NY",
-//   },
-//   {
-//     title: "Event 4",
-//     image: "/images/event4.png",
-//     date: "2026-03-03",
-//     time: "10:00 AM",
-//     slug: "event-4",
-//     location: "New York, NY",
-//   }
-// ]
-
 export default function Page() {
   const [events, setEvents] = useState([]);
   const getEvents = async () => {
     const response = await fetch("http://localhost:5000/api/events");
     const data = await response.json();
     setEvents(data.data);
-    console.log(data);
   }
   useEffect(() => {
     getEvents();
   }, []);   
-  console.log(events);
+  
   return <section>
     <h1 className="text-center">The Hub for Every Dev <br /> Event You Must't Miss</h1>
     <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
